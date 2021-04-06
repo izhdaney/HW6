5f0b5c07c2f89efab76de2ab034cbcf9b9167ced
izhdaney <79247431+izhdaney@users.noreply.github.com>
2021-04-06 14:48:13 +0300

    14-47

--- Program.cs
+++ Program.cs
@@ -6,104 +6,171 @@ namespace HW6
     {
         static void Main(string[] args)
         {
-            //Task1();
-            //Task2();
-            //Task3();
-            //Task4();
-            //Task5();
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+            Task5();
             Task6();
-            //getResultTask7();
+            Task7();
+            Task7_2();
+            
 
         }
 
         private static void Task1()
         {
-            int [,] arr = getNewArray();
-
-            arr = addArrayElement(arr);
-
-            int minElemenet = getResultTask1(arr);
-
-            foreach (var el in arr)
+            Console.WriteLine("Task 1");
+            do
             {
-                Console.Write($"{el}\t");
-            }
 
-            Console.WriteLine($"\nMin Element is {minElemenet}");
+                int [,] arr = getNewArray();
+
+                arr = addArrayElement(arr);
+
+                int minElemenet = getResultTask1(arr);
+
+                foreach (var el in arr)
+                {
+                    Console.Write($"{el}\t");
+                }
+
+
+                Console.WriteLine($"\nMin Element is {minElemenet}. Continue? (y=yes)");
+            } while (Console.ReadLine() == "y");
         }
 
         private static void Task2()
         {
-            int[,] arr = getNewArray();
-
-            arr = addArrayElement(arr);
-
-            int maxElemenet = getResultTask2(arr);
-
-            foreach (var el in arr)
+            Console.WriteLine("Task 2");
+            do
             {
-                Console.Write($"{el}\t");
-            }
 
-            Console.WriteLine($"\nMax Element is {maxElemenet}");
+                int[,] arr = getNewArray();
+
+                arr = addArrayElement(arr);
+
+                int maxElemenet = getResultTask2(arr);
+
+                foreach (var el in arr)
+                {
+                    Console.Write($"{el}\t");
+                }
+
+
+                Console.WriteLine($"\nMax Element is {maxElemenet}. Continue? (y=yes)");
+            } while (Console.ReadLine() == "y");
         }
 
         private static void Task3()
         {
-            int[,] arr = getNewArray();
+            Console.WriteLine("Task 3");
 
-            arr = addArrayElement(arr);
-
-            string minElemenet = getResultTask3(arr);
-
-            foreach (var el in arr)
+            do
             {
-                Console.Write($"{el}\t");
-            }
 
-            Console.WriteLine($"\n{minElemenet}");
+                int[,] arr = getNewArray();
+
+                arr = addArrayElement(arr);
+
+                string minElemenet = getResultTask3(arr);
+
+                foreach (var el in arr)
+                {
+                    Console.Write($"{el}\t");
+                }
+
+                Console.WriteLine($"\nIndex of min element in array {minElemenet}. Continue? (y=yes)");
+
+            } while (Console.ReadLine() == "y");
         }
 
         private static void Task4()
         {
-            int[,] arr = getNewArray();
+            Console.WriteLine("Task 4");
 
-            arr = addArrayElement(arr);
-
-            string maxElemenet = getResultTask4(arr);
-
-            foreach (var el in arr)
+            do
             {
-                Console.Write($"{el}\t");
-            }
 
-            Console.WriteLine($"\n{maxElemenet}");
+                int[,] arr = getNewArray();
+
+                arr = addArrayElement(arr);
+
+                string maxElemenet = getResultTask4(arr);
+
+                foreach (var el in arr)
+                {
+                    Console.Write($"{el}\t");
+                }
+
+                Console.WriteLine($"\nIndex of max element in array {maxElemenet}. Continue? (y=yes)");
+
+            } while (Console.ReadLine() == "y");
         }
 
         private static void Task5()
         {
-            int[,] arr = getNewArray();
-
-            arr = addArrayElement(arr);
-
-            int count = getResultTask5(arr);
-
-            foreach (var el in arr)
+            Console.WriteLine("Task 5");
+            do
             {
-                Console.Write($"{el}\t");
-            }
 
-            Console.WriteLine($"\n Count is {count}");
+                int[,] arr = getNewArray();
+
+                arr = addArrayElement(arr);
+
+                int count = getResultTask5(arr);
+
+                foreach (var el in arr)
+                {
+                    Console.Write($"{el}\t");
+                }
+
+
+                Console.WriteLine($"\nThe number of array elements that are larger than all their neighbors at the same time {count}. Continue? (y=yes)");
+
+            } while (Console.ReadLine() == "y");
+
         }
 
         private static void Task6()
         {
-            int number = numberReadTask6();
+            Console.WriteLine("Task 6");
+            do
+            {
 
-            string result = getResultTask6(number);
+                int number = numberReadTask6();
 
-            Console.WriteLine($"Result is - {result}");
+                string result = getResultTask6(number);
 
+
+                Console.WriteLine($"\nResult is - {result}. Continue? (y=yes)");
+
+            } while (Console.ReadLine() == "y");
+        }
+
+        private static void Task7()
+        {
+            Console.WriteLine("Task 7");
+
+            do
+            {
+                int result = getResultTask7();
+
+                Console.WriteLine($"\nResult is - {result}. Continue? (y=yes)");
+
+            } while (Console.ReadLine() == "y");
+        }
+
+        private static void Task7_2()
+        {
+            Console.WriteLine("Task 7_2");
+            do
+            {
+                int result = getResultTask7_2();
+
+                Console.WriteLine($"\nResult is - {result}. Continue? (y=yes)");
+
+            } while (Console.ReadLine() == "y");
 
         }
 
@@ -257,6 +324,211 @@ namespace HW6
             return count;
         }
 
+        private static string getResultTask6(int n)
+        {
+            string[] arr1 = new string[20] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            string[] arr2 = new string[8] { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+            string result = "";
+
+            if (n > 0 && n < 20)
+            {
+                result = $"{arr1[n]}";
+            }
+            else if (n < 99)
+            {
+                if (n == 0)
+                {
+                    result = $"{arr1[n]}";
+                }
+                else if (n % 10 == 0)
+                {
+                    int a = n / 10;
+                    result = $"{arr2[a - 2]}";
+                }
+                else
+                {
+                    int a = n % 10;
+                    int b = n / 10;
+                    result = $"{arr2[b - 2]}-{arr1[a]}";
+                }
+            }
+            else if (n == 100 || n == 200 || n == 300 || n == 400 || n == 500 || n == 600 || n == 700 || n == 800 || n == 900)
+            {
+                n = n / 100;
+                result = $"{arr1[n]} hundred";
+            }
+            else if (n % 100 < 20)
+            {
+                int a = n % 100;
+                int b = n / 100;
+
+                result = $"{arr1[b]} hundred and {arr1[a]}";
+            }
+            else
+            {
+                if (n % 10 == 0)
+                {
+                    int a = n / 100;
+                    n = n / 10;
+                    int b = n % 10;
+                    result = $"{arr1[a]} hundred and {arr2[b - 2]}";
+                }
+                else
+                {
+                    int c = n % 10;
+                    n = n / 10;
+                    int b = n % 10;
+                    int a = n / 10;
+
+                    result = $"{arr1[a]} hundred and {arr2[b - 2]}-{arr1[c]}";
+                }
+            }
+
+
+            return result;
+        }
+
+        private static int getResultTask7()
+        {
+            Console.WriteLine("Enter the input, which contains a number written in words (0 - 999).");
+            string[] number = Console.ReadLine().Split(" ");
+
+            int n = number.Length;
+
+            int[] arrNum1 = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] arrNum2 = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            int[] arrNum3 = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] arrNum4 = new int[] { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+            int[] arrNum5 = new int[] { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+
+            string[] arr1 = new string[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            string[] arr2 = new string[] { "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+            string[] arr3 = new string[] { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+
+            int a = 0;
+            int b = 0;
+            int c = 0;
+
+            string result = "";
+
+            if (n == 5)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (number[0] == arr1[i])
+                    {
+                        a = arrNum1[i];
+                    }
+                    if (number[3] == arr2[i])
+                    {
+                        b = arrNum1[i];
+                    }
+                    if (number[4] == arr1[i])
+                    {
+                        c = arrNum1[i];
+                    }
+                }
+                result = $"{a}{b}{c}";
+            }
+            else if (n == 4)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (number[0] == arr1[i])
+                    {
+                        a = arrNum1[i];
+                    }
+                    if (number[3] == arr3[i])
+                    {
+                        b = arrNum2[i];
+                        c = arrNum3[i];
+                    }
+                    else if (number[3] == arr2[i])
+                    {
+                        b = arrNum1[i];
+                    }
+                    else if (number[3] == arr1[i])
+                    {
+                        c = arrNum1[i];
+                    }
+
+                }
+                result = $"{a}{b}{c}";
+            }
+            else if (n == 2)
+            {
+                if (number[1] == "hundred")
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (number[0] == arr1[i])
+                        {
+                            a = arrNum4[i];
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (number[0] == arr2[i])
+                        {
+                            a = arrNum1[i];
+                        }
+                        if (number[1] == arr1[i])
+                        {
+                            b = arrNum1[i];
+                        }
+
+                    }
+                }
+                result = $"{a}{b}";
+            }
+            else
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (number[0] == arr1[i])
+                    {
+                        a = arrNum1[i];
+                    }
+                    else if (number[0] == arr3[i])
+                    {
+                        a = arrNum5[i];
+                    }
+                    else if (number[0] == arr2[i])
+                    {
+                        a = arrNum4[i];
+                    }
+                }
+                result = $"{a}";
+            }
+
+
+            int outNumber = Convert.ToInt32(result);
+            return outNumber;
+
+        }
+
+        private static int getResultTask7_2()
+        {
+            Console.WriteLine("Enter the input, which contains a number written in words (0 - 999).");
+            string n = Console.ReadLine();
+            var result = 0;
+
+            for (int i = 0; i < 999; i++)
+            {
+                string a = getResultTask6(i);
+                if (a == n)
+                {
+                    result = i;
+                    break;
+                }
+            }
+            return result;
+        }
+
         private static int[,] getNewArray()
         {
             string columnMessage = "Please enter array column length - int > 0";
@@ -348,187 +620,6 @@ namespace HW6
             return returnValue;
         }
 
-        private static string getResultTask6(int n)
-        {
-            string[] arr1 = new string[20] {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-            string[] arr2 = new string[8] { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
-            string result = "";
-
-            if (n > 0 && n < 20)
-            {
-                result = $"{arr1[n]}";
-            }
-            else if (n < 99)
-            {
-                if (n % 10 == 0)
-                {
-                    int a = n / 10;
-                    result = $"{arr2[a - 2]}";
-                }
-                else
-                {
-                    int a = n % 10;
-                    int b = n / 10;
-                    result = $"{arr2[b - 2]}-{arr1[a]}";
-                }
-            }
-            else if (n == 100 || n == 200 || n == 300 || n == 400 || n == 500 || n == 600 || n == 700 || n == 800 || n == 900)
-            {
-                n = n / 100;
-                result = $"{arr1[n]} hundred";
-            }
-            else if (n % 100 < 20)
-            {
-                int a = n % 100;
-                int b = n / 100;
-
-                result = $"{arr1[b]} hundred and {arr1[a]}";
-            }
-            else
-            {
-                if (n % 10 == 0)
-                {
-                    int a = n / 100;
-                    n = n / 10;
-                    int b = n % 10;
-                    result = $"{arr1[a]} hundred and {arr2[b - 2]}";
-                }
-                else
-                {
-                    int c = n % 10;
-                    n = n / 10;
-                    int b = n % 10;
-                    int a = n / 10;
-
-                    result = $"{arr1[a]} hundred and {arr2[b - 2]}-{arr1[c]}";
-                }
-            }
-
-
-            return result;
-        }
-
-        private static void getResultTask7()
-        {
-            //7.Вводим строку, которая содержит число, написанное прописью(0 - 999). Получить само число. 
-            Console.WriteLine("Enter the input, which contains a number written in words (0 - 999).");
-            string[] number = Console.ReadLine().Split(" ");
-
-            int n = number.Length;
-
-            int[] arrNum1 = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            int[] arrNum2 = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-            int[] arrNum3 = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            int[] arrNum4 = new int[] { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90 };
-            int[] arrNum5 = new int[] {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-
-        string[] arr1 = new string[] {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-            string[] arr2 = new string[] {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-            string[] arr3 = new string[] { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-
-            int a = 0;
-            int b = 0;
-            int c = 0;
-
-            string result = "";
-
-            if (n == 5)
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    if (number[0] == arr1[i])
-                    {
-                        a = arrNum1[i];
-                    }
-                    if (number[3] == arr2[i])
-                    {
-                        b = arrNum1[i];
-                    }
-                    if (number[4] == arr1[i])
-                    {
-                        c = arrNum1[i];
-                    }
-                }
-                result = $"{a}{b}{c}";
-            }
-            else if (n == 4) 
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    if (number[0] == arr1[i])
-                    {
-                        a = arrNum1[i];
-                    }
-                    if (number[3] == arr3[i])
-                    {
-                        b = arrNum2[i];
-                        c = arrNum3[i];
-                    }
-                    else if (number[3] == arr2[i])
-                    {
-                        b = arrNum1[i];
-                    }
-                    else if (number[3] == arr1[i])
-                    {
-                        c = arrNum1[i];
-                    }
-
-                }
-                result = $"{a}{b}{c}";
-            }
-            else if (n == 2)
-            {
-                if (number[1] == "hundred")
-                {
-                    for (int i = 0; i < 10; i++)
-                    {
-                        if (number[0] == arr1[i])
-                        {
-                            a = arrNum4[i];
-                        }
-                    }
-                }
-                else
-                {
-                    for (int i = 0; i < 10; i++)
-                    {
-                        if (number[0] == arr2[i])
-                        {
-                            a = arrNum1[i];
-                        }
-                        if (number[1] == arr1[i])
-                        {
-                            b = arrNum1[i];
-                        }
-
-                    }
-                }
-                result = $"{a}{b}";
-            }
-            else
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    if (number[0] == arr1[i])
-                    {
-                        a = arrNum1[i];
-                    }
-                    else if (number[0] == arr3[i])
-                    {
-                        a = arrNum5[i];
-                    }
-                    else if (number[0] == arr2[i])
-                    {
-                        a = arrNum4[i];
-                    }
-                }
-                result = $"{a}";
-            }
-
-
-            Console.WriteLine(result);
-
-        }
     }
 }
